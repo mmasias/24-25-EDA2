b8dbfd4b00a77b41dc262e72a009a9c97e201f50
pareyor <pablo.reyortiz@alumnos.uneatlantico.es>
2025-02-04 11:36:59 +0100

    clase Pantalla

--- entregas/ReyOrtizPablo/Reto-000/Entrelazado.java
+++ entregas/ReyOrtizPablo/Reto-000/Entrelazado.java
@@ -1,3 +1,14 @@
+import java.util.Map;
+
 public class Entrelazado {
-    
+
+    private Map<Character,Map<Character,Character>> colorTabla;
+
+    public Entrelazado() {
+        
+    }
+    public char combinarColores(char color1, char color2){
+
+           
+    }
 }
--- entregas/ReyOrtizPablo/Reto-000/Pantalla.java
+++ entregas/ReyOrtizPablo/Reto-000/Pantalla.java
@@ -1,3 +1,39 @@
 public class Pantalla {
-    
+    private Frame frame1;
+    private Frame frame2;
+    private int currentFrame = 0;
+
+    public Pantalla(Frame frame1, Frame frame2) {
+        this.frame1 = frame1;
+        this.frame2 = frame2;
+    }
+
+    public void cambiarFrame() {
+        currentFrame = (currentFrame + 1) % 2;
+    }
+
+    public void actualizarPantalla() {
+        if (currentFrame == 0) {
+            frame1.renderizar();
+        } else {
+            frame2.renderizar();
+        }
+    }
+
+    public char[][] combinarFrames() {
+        int altura = frame1.getAltura();
+        int anchura = frame1.getAnchura();
+        char[][] combinado = new char[altura][anchura];
+        Entrelazado entrelazado = new Entrelazado();
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < anchura; j++) {
+                char color1 = frame1.getPixel(j, i);
+                char color2 = frame2.getPixel(j, i);
+                combinado[i][j] = entrelazado.combinarColores(color1, color2);
+            }
+        }
+
+        return combinado;
+    }
 }
