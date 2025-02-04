1752a124445a41582539fc831673915f188fdaed
Alejandrojuarez0105 <alejandrojuarez0105@gmail.com>
2025-02-05 00:50:33 +0100

    La solución que deberia ser
    Se dejó como comentario la solución que debería de ser correcta para que funcione el codigo UNICAMENTE con lo que plantee en el exámen

--- entregas/src/segundoEjercicio/ResultadoVisual.java
+++ entregas/src/segundoEjercicio/ResultadoVisual.java
@@ -20,3 +20,52 @@ public class ResultadoVisual {
         }
     }
 }
+
+/*
+ * package segundoEjercicio;
+ * 
+ * public class ResultadoVisual {
+ * private char[][] contenido;
+ * 
+ * public ResultadoVisual(int ancho, int alto) {
+ * contenido = new char[ancho][alto];
+ * }
+ * 
+ * public void combinar(Frame1 frame1, Frame2 frame2) {
+ * char[][] f1 = frame1.contenido;
+ * char[][] f2 = frame2.contenido;
+ * 
+ * for (int i = 0; i < f1.length; i++) {
+ * for (int j = 0; j < f1[0].length; j++) {
+ * contenido[i][j] = combinarColores(f1[i][j], f2[i][j]);
+ * }
+ * }
+ * mostrarResultado();
+ * }
+ * 
+ * private char combinarColores(char c1, char c2) {
+ * return switch ("" + c1 + c2) {
+ * case ".." -> '.';
+ * case ".#", "#." -> '@';
+ * case ".+", "+." -> ':';
+ * case ".*", "*." -> ';';
+ * case "##" -> '#';
+ * case "#+", "+#" -> '=';
+ * case "#*", "*#" -> '&';
+ * case "++" -> '+';
+ * case "+*", "*+" -> '$';
+ * case "**" -> '*';
+ * default -> '?';
+ * };
+ * }
+ * 
+ * private void mostrarResultado() {
+ * for (char[] fila : contenido) {
+ * for (char pixel : fila) {
+ * System.out.print(pixel + " ");
+ * }
+ * System.out.println();
+ * }
+ * }
+ * }
+ */
