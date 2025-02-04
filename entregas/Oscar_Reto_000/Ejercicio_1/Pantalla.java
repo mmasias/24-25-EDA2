1f36a49b58de92f1e663124f8270f4669fb9413e
OscarPineda9 <oscar.pineda@alumnos.uneatlantico.es>
2025-02-04 20:28:53 +0100

    Ejercicio 1

---
+++ entregas/Oscar_Reto_000/Ejercicio_1/Pantalla.java
@@ -0,0 +1,13 @@
+package entregas.Oscar_Reto_000.Ejercicio_1;
+class Pantalla {
+    private DobleBuffer buffer;
+    
+    public Pantalla(int ancho, int alto, int resolucion) {
+        buffer = new DobleBuffer(ancho, alto, resolucion);
+    }
+    
+    public void alternarFrames() {
+        
+        System.out.println("Alternando frames...");
+    }
+}
