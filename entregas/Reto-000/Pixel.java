02e6a80f1ef3c2edeb7fa75c154be29aeaa4883a
pareyor <pablo.reyortiz@alumnos.uneatlantico.es>
2025-02-04 11:04:51 +0100

    paso clase pixel a codigo

---
+++ entregas/Reto-000/Pixel.java
@@ -0,0 +1,19 @@
+public class Pixel {
+    private int x;
+    private int y;
+    private char color;
+    public Pixel(int x, int y, char color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+    
+    public void setColor(char color) {
+        this.color = color;
+    }
+    public char getColor() {
+        return color;
+    }
+   
+    
+}
