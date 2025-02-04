7f4848389bf2f38cb183a541cde1acb9d252621a
OscarPineda9 <oscar.pineda@alumnos.uneatlantico.es>
2025-02-04 22:40:34 +0100

    Ejercicio 2

---
+++ entregas/Oscar_Reto_000/Ejercicio_2/Resultados.java
@@ -0,0 +1,15 @@
+package entregas.Oscar_Reto_000.Ejercicio_2;
+
+class Resultados {
+    private Frame frame1;
+    private Frame frame2;
+    
+    public Resultados(Frame frame1, Frame frame2) {
+        this.frame1 = frame1;
+        this.frame2 = frame2;
+    }
+    
+    public void mostrarResultados() {
+        System.out.println("Mostrando resultados de los frames...");
+    }
+}
