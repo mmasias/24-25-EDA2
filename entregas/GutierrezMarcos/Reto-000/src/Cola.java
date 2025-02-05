813489ccc951d3510706349abcd055141b62c780
marcosgutierrez6 <marcos.gutierrez1@alumnos.uneatlantico.es>
2025-02-05 12:53:00 +0100

    Fix Funciona

--- entregas/GutierrezMarcos/Reto-000/src/Cola.java
+++ entregas/GutierrezMarcos/Reto-000/src/Cola.java
@@ -25,23 +25,23 @@ public class Cola {
     }
 
     public Frame[] desenqueue(){
-        if (tieneDosFrames()) {
+        if (tamaño >= 2) {
             Frame[] frames = new Frame[2];
-            NodoFrame actual = primero;
-            while (actual.getSiguiente() != null && actual.getSiguiente().getSiguiente() != null) {
-                actual = actual.getSiguiente();
+            frames[0] = primero;
+            frames[1] = primero.getSiguiente();
+    
+            if (primero.getSiguiente() != null) {
+                primero = primero.getSiguiente().getSiguiente();
+            } else {
+                primero = null;
             }
-            frames[0] = actual.getSiguiente();
-            frames[1] = actual.getSiguiente().getSiguiente();
-            NodoFrame siguiente = actual.getSiguiente();
-            siguiente = null;
-            this.tamaño -=2;
+            tamaño -= 2;
             return frames;
-        }else{
+        } else {
             return null;
         }
-
     }
+    
 
     public Frame[] peek(){
         if (tieneDosFrames()) {
--- entregas/GutierrezMarcos/Reto-000/src/Pantalla.java
+++ entregas/GutierrezMarcos/Reto-000/src/Pantalla.java
@@ -4,14 +4,12 @@ public class Pantalla{
     private Grafica grafica;
 
     public void mostrar(){
-        
         this.grafica = new Grafica();
-
+    
         while(true){
             frames = grafica.devolverDosFrames();
-
-            if(frames[0]!=null && frames[1]!=null){
-
+    
+            if(frames != null && frames[0] != null && frames[1] != null){
                 for (int i = 0; i < 3; i++) {
                     for (int j = 0; j < 7; j++) {
                         System.out.print(frames[0].devolverPixeles()[i][j].devolverColor().devolverRepresentacion() + " ");
@@ -20,9 +18,15 @@ public class Pantalla{
                         System.out.print(frames[1].devolverPixeles()[i][j].devolverColor().devolverRepresentacion() + " ");
                     }
                     System.out.println();
-    
                 }
+            } else {
+                System.out.println("Aún no se han generado los frames suficientes.");
             }
         }
+    }
+    
+
+    public static void main(String[] args) {
+        new Pantalla().mostrar();
     }
 }
