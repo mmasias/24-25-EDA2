a073b9ea4b5ac69e1fb685f13102d6fdb41acdf6
SdeCos <saul.decossanchez@alumnos.uneatlantico.es>
2025-02-05 13:12:28 +0100

    Codigo entrega 001

---
+++ entregas/deCosSaul/reto-001/src/Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    Pantalla pantalla = new Pantalla();
+
+    pantalla.addFrame(0);
+    pantalla.addFrame(1);
+
+    pantalla.mostrarFrame(0);
+    pantalla.mostrarFrame(1);
+  }
+}
