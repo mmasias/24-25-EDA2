25ffde09cccdb0349e6e14e987f6b38723b773d7
FernandoWilliam26 <fernando.calvo@alumnos.uneatlantico.es>
2025-02-05 17:18:28 +0100

    Entrega Reto000

---
+++ entregas/calvoFernando/reto000/src/Frame.java
@@ -0,0 +1,21 @@
+public class Frame {
+
+    private Pixel[][] pixeles;
+
+    public Frame(){
+        pixeles = new Pixel[3][7];
+        rellenarPixeles();
+    }
+
+    public void rellenarPixeles(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 7; j++) {
+                pixeles[i][j] = new Pixel();
+            }
+        }
+    }
+
+    public Pixel[][] devolverPixeles(){
+        return this.pixeles;
+    }
+}
