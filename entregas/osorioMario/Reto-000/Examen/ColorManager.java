68b2ea2c00c38f0c4df443f59fcee8e8a9404ccb
Maritoz <maritox47@gmail.com>
2025-02-05 12:49:14 +0100

    Limpieza

--- entregas/osorioMario/Reto-000/Examen/ColorManager.java
+++
@@ -1,7 +0,0 @@
-public class ColorManager {
-    private String[] colors = {"Negro", "Blanco", "Cyan", "Magenta"};
-
-    public String mixColors(String color1, String color2) {
-        return color1 + "+" + color2;
-    }
-}
--- entregas/osorioMario/Reto-000/Examen/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        GraphicsSystem system = new GraphicsSystem(5, 5, 2000);
-
-        // Simulación de actualización de píxeles individuales
-        system.updatePixel(2, 2, "Blanco");
-        system.render();
-    }
-}
