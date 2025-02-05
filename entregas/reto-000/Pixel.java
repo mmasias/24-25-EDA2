d507201b89229fc6853f7d9885fe080e198e2a86
andreslb9 <144810780+andreslb9@users.noreply.github.com>
2025-02-05 12:12:30 +0100

    Clase pixel

---
+++ entregas/reto-000/Pixel.java
@@ -0,0 +1,19 @@
+public class Pixel{
+  private boolean estado;
+  private String color;
+
+public boolean getEstado() {
+    return estado;
+}
+public void setEstado(boolean estado) {
+    this.estado = estado;
+}
+public String getColor() {
+    return color;
+}
+public void setColor(String color) {
+    this.color = color;
+}
+
+
+}
