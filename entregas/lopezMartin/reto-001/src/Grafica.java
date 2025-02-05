54c8eeeb86e75952584ee3034c7939bf8b1a58ff
martin <martin.lopez@alumnos.uneatlantico.es>
2025-02-05 13:21:12 +0100

    avance refactorizacion

---
+++ entregas/lopezMartin/reto-001/src/Grafica.java
@@ -0,0 +1,32 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Grafica {
+
+    private final int memoriaKB = 16;
+    private final int tasaHZ = 60;
+    private Cola colaFrames;
+
+    public Grafica(){
+        this.colaFrames = new Cola();
+    }
+
+    public void generarFrames(){
+
+        Timer temporizador = new Timer();
+        
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                colaFrames.enqueue();
+            }
+        };
+        temporizador.scheduleAtFixedRate(tarea, 0, 1000);
+
+    }
+
+    public Frame[] devolverDosFrames(){
+        return colaFrames.desenqueue();
+    }
+    
+}
---
+++ entregas/lopezMartin/reto-001/src/Pantalla.java
@@ -0,0 +1,33 @@
+public class Pantalla{
+
+    private Frame[] buffer;
+    private Grafica grafica;
+
+    public Pantalla(){
+        this.grafica = new Grafica();
+        buffer = null;
+    }
+
+    public void mostrar(){
+
+        grafica.generarFrames();
+
+        while(true){
+            buffer = grafica.devolverDosFrames();
+
+            if(buffer[0]!=null && buffer[1]!=null){
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 7; j++) {
+                        System.out.print(buffer[0].devolverPixeles()[i][j].devolverColor().devolverRepresentacion() + " ");
+                    } 
+                    for (int j = 0; j < 7; j++) {
+                        System.out.print(buffer[1].devolverPixeles()[i][j].devolverColor().devolverRepresentacion() + " ");
+                    }
+                    System.out.println();
+    
+                }
+            }
+        }
+    }
+}
