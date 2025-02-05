f319c3ba25219031c3c9c23698c57d1bac0eaf5b
enmabry <bryanbaquedano11@gmail.com>
2025-02-05 02:11:20 +0100

    Termine de implementar mi clase frame
    implementé los métodos: buscar, resetear, actualizar.

--- entregas/baquedanoBryan/reto-000/src/Frame.java
+++ entregas/baquedanoBryan/reto-000/src/Frame.java
@@ -11,5 +11,38 @@ public class Frame {
             }
         }
     }
-    
+
+    public Pixel obtenerPixel(int fila, int columna){
+        return this.pixeles[fila][columna];
+    }
+
+    public Pixel buscarPixel(char caracter) {
+        for (int i = 0; i < pixeles.length; i++) {
+            for (int j = 0; j < pixeles[i].length; j++) {
+                if (pixeles[i][j].getNumeral() == caracter || 
+                    pixeles[i][j].getPunto() == caracter || 
+                    pixeles[i][j].getMas() == caracter) {
+                    return pixeles[i][j];
+                }
+            }
+        }
+        return null;
+    }
+
+    public void resetearFrame() {
+        for (int i = 0; i < pixeles.length; i++) {
+            for (int j = 0; j < pixeles[i].length; j++) {
+                pixeles[i][j] = new Pixel('#', '.', '+');
+            }
+        }
+    }
+
+    public void actualizarPixel(int fila, int columna, Pixel nuevoPixel) {
+        if (fila >= 0 && fila < pixeles.length && columna >= 0 && columna < pixeles[0].length) {
+            pixeles[fila][columna] = nuevoPixel;
+        } else {
+            System.out.println("Error: Posición fuera de los límites del Frame.");
+        }
+    }
+      
 }
