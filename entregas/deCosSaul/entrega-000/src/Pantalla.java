104c60b323f93f3a134adbb2e2684895fa4ed07a
SdeCos <saul.decossanchez@alumnos.uneatlantico.es>
2025-02-05 08:03:19 +0100

    entrega 000

---
+++ entregas/deCosSaul/entrega-000/src/Pantalla.java
@@ -0,0 +1,28 @@
+class Pantalla {
+  int posicion;
+  Frame[] pantalla = new Frame[2];
+
+  public Frame generarFrame(int posicion) {
+    Frame frame = new Frame(21);
+
+    for (int i = 0; i < 21; i++) {
+      int randomNumber = (int) (Math.random() * 4);
+      frame.modificarPixel(i, randomNumber);
+    }
+
+    return frame;
+
+  }
+
+  public void addFrame(int pos) {
+    pantalla[pos] = generarFrame(pos);
+  }
+
+  public void mostrarFrame(int posicion) {
+    pantalla[posicion].imprimir();
+  }
+
+  public void modificarPixel(int frame, int pos, int nuevoValor) {
+    pantalla[frame].modificarPixel(pos, nuevoValor);
+  }
+}
