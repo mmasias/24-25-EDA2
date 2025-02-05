5cb641cc3a0a2d853f4aeab6ff51d44f617f2750
Maritoz <maritox47@gmail.com>
2025-02-05 03:03:21 +0100

    Examen

---
+++ entregas/osorioMario/Reto-000/Examen/ColorManager.java
@@ -0,0 +1,7 @@
+public class ColorManager {
+    private String[] colors = {"Negro", "Blanco", "Cyan", "Magenta"};
+
+    public String mixColors(String color1, String color2) {
+        return color1 + "+" + color2;
+    }
+}
