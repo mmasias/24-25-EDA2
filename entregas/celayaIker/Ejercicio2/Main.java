f96af2ac5b84990566f7c4e270bcfaa5c866513e
ikercelaya <iker.celaya@alumnos.uneatlantico.es>
2025-02-09 23:33:19 +0100

    Ejercicio2

--- entregas/celayaIker/Ejercicio2/Main.java
+++ entregas/celayaIker/Ejercicio2/Main.java
@@ -4,9 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Pantalla pantalla = new Pantalla();
 
-        pantalla.modificarPixel(0, 10, 10, 255);
+        pantalla.modificarPixel(0, 2, 0, 255);
+        pantalla.modificarPixel(0, 4, 1, 128);
+        pantalla.modificarPixel(0, 0, 2, 255);
 
-        pantalla.alternarColores();
-        pantalla.alternarColores(); 
+        pantalla.mostrarFrame(0);
+
+        pantalla.modificarPixel(1, 0, 0, 64);
+        pantalla.modificarPixel(1, 4, 1, 64);
+        pantalla.modificarPixel(1, 2, 2, 64);
+
+        pantalla.mostrarFrame(1);
+
+        pantalla.combinarFrames();
     }
 }
