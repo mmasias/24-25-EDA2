970b1ba62e8f2e13477323189c6a815121437c8c
JP <144849667+JPMoralesl004@users.noreply.github.com>
2025-02-09 14:17:39 +0100

    Reto-001

---
+++ entregas/Reto-001/MoralesJosePablo/src/Ejercicio 1/Buffer.java
@@ -0,0 +1,5 @@
+public class Buffer {
+
+    public void mostrarFrameEscogido(Frame1 f1, Frame2 f2) {
+    }
+}
---
+++ entregas/Reto-001/MoralesJosePablo/src/Ejercicio 1/Pantalla.java
@@ -0,0 +1,44 @@
+public class Pantalla {
+
+    @SuppressWarnings("unused")
+    private String nombre;
+
+    public Pantalla(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Frame1 getFrame1(char[][] contenido, Pixel posicion) {
+        return new Frame1(contenido, posicion);
+    }
+
+    public Frame2 getFrame2(char[][] contenido, Pixel posicion) {
+        return new Frame2(contenido, posicion);
+    }
+
+    public void iniciarAplicacion() {
+        int width = 7, height = 3;
+        Pixel p1 = new Pixel(2, 2);
+        Pixel p2 = new Pixel(3, 3);
+
+        Frame1 f1 = new Frame1(new char[][] {
+            { '.', '.', '.', '#', '#', '.', '.' },
+            { '+', '+', '.', '.', '.', '.', '.' },
+            { '#', '#', '#', '.', '.', '.', '.' }
+        }, p1);
+
+        Frame2 f2 = new Frame2(new char[][] {
+            { '.', '.', '.', '#', '#', '.', '.' },
+            { '+', '+', '.', '.', '.', '.', '.' },
+            { '#', '#', '#', '.', '.', '.', '.' }
+        }, p2);
+
+        ResultadoVisual rv = new ResultadoVisual(width, height);
+        rv.combinar(f1, f2);
+        rv.mostrarResultado();
+    }
+
+    public static void main(String[] args) {
+        Pantalla pantalla = new Pantalla("ClaseEjemplo");
+        pantalla.iniciarAplicacion();
+    }
+}
