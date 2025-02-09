501fc019fd319b0aa47818783ffb71e51754701d
jerdier <javier.rodriguez4@alumnos.uneatlantico.es>
2025-02-09 20:00:43 +0100

    Reto-001 codigo funcional y corregido para que represente dos frames que juntos den la sensacion de una imagen mas ancha

--- entregas/rodriguezJavier/Reto-001/Display.java
+++ entregas/rodriguezJavier/Reto-001/Display.java
@@ -1,3 +1,4 @@
+
 class Display {
 
     private Pixel[][] displayPixels;
@@ -20,7 +21,29 @@ class Display {
     }
 
     public static void main(String[] args) {
-        Display display = new Display(10, 20);
-        display.render();
+        Display display1 = new Display(5, 10);
+        Display display2 = new Display(5, 10);
+
+        for (int i = 0; i < display1.displayPixels.length; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < display1.displayPixels[i].length; j++) {
+                    if (display1.displayPixels[i][j] != null) {
+                        System.out.print(display1.displayPixels[i][j].getValue() + " ");
+                    } else {
+                        System.out.print("# ");
+                    }
+                }
+            } else {
+                System.out.print("  ");
+                for (int j = 0; j < display2.displayPixels[i].length; j++) {
+                    if (display2.displayPixels[i][j] != null) {
+                        System.out.print(display2.displayPixels[i][j].getValue() + " ");
+                    } else {
+                        System.out.print("# ");
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
