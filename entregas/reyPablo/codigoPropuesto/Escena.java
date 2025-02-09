62f2d51c29836813cfc285623243edcb258ba705
pareyor <pablo.reyortiz@alumnos.uneatlantico.es>
2025-02-09 16:57:35 +0100

    implementacion clase Escena

---
+++ entregas/reyPablo/codigoPropuesto/Escena.java
@@ -0,0 +1,38 @@
+package entregas.reyPablo.codigoPropuesto;
+
+public class Escena {
+    private int indiceActual;
+    private Frame[] frames;
+
+    public Escena(int anchura, int altura, int numeroFrames) {
+        this.frames = new Frame[numeroFrames];
+        for (int i = 0; i < frames.length; i++) {
+            this.frames[i] = new Frame(anchura, altura);
+        }
+        this.indiceActual = 0;
+    }
+
+    public void siguiente() {
+
+        this.indiceActual = (this.indiceActual + 1) % frames.length;
+
+    }
+
+    public Frame getFrame() {
+
+        return this.frames[indiceActual];
+
+    }
+
+    public Frame getFrame(int indice) {
+
+        if (indice > 0 && indice < frames.length) {
+            return this.frames[indice];
+        }
+        return null;
+    }
+
+    public int numeroFrames(){
+        return frames.length;
+    }
+}
