db6505ed1c1602fe3cb2f037c57dc01419c64d7d
martin <martin.lopez@alumnos.uneatlantico.es>
2025-02-10 11:37:50 +0100

    avance

--- entregas/sesion10_02_2025/src/Demo.java
+++ entregas/sesion10_02_2025/src/Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
-        GestorPantalla pantalla = new GestorPantalla(8,3);
+        GestorPantalla pantalla = new GestorPantalla(new Resolucion(8, 3));
 
         pantalla.establecerPixel(new Coordenada(7,2),2);
         pantalla.establecerPixel(new Coordenada(6,1),2);
