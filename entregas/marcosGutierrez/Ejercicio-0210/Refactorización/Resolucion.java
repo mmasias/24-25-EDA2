471cf3cbfd103c30f413520537bd588fd2d7146b
marcosgutierrez6 <marcos.gutierrez1@alumnos.uneatlantico.es>
2025-02-10 11:30:02 +0100

    Clase resolucion

---
+++ entregas/marcosGutierrez/Ejercicio-0210/Refactorización/Resolucion.java
@@ -0,0 +1,43 @@
+public class Resolucion {
+    private final int ancho;
+    private final int alto;
+    private final Pixel[][] frameIzquierdo;
+    private final Pixel[][] frameDerecho;
+    
+    public Resolucion(int ancho, int alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+        
+        frameIzquierdo = new Pixel[ancho/2][alto];
+        frameDerecho = new Pixel[ancho/2][alto];
+        
+        for (int y = 0; y < alto; y++) {
+            for (int x = 0; x < ancho/2; x++) {
+                frameIzquierdo[x][y] = new Pixel();
+                frameDerecho[x][y] = new Pixel();
+            }
+        }
+    }
+    
+    public void setPixel(Coordenada coordenada, int color) {
+        if (coordenada.getX() >= ancho/2) {
+            frameDerecho[coordenada.getX() - ancho/2][coordenada.getY()].setColor(color);
+        } else {
+            frameIzquierdo[coordenada.getX()][coordenada.getY()].setColor(color);
+        }
+    }
+    
+    public void renderizar() {
+        for (int y = 0; y < alto; y++) {
+            // Renderizar frame izquierdo
+            for (int x = 0; x < ancho/2; x++) {
+                System.out.print(frameIzquierdo[x][y].getColor() + "|");
+            }
+            // Renderizar frame derecho
+            for (int x = 0; x < ancho/2; x++) {
+                System.out.print(frameDerecho[x][y].getColor() + "|");
+            }
+            System.out.println();
+        }
+    }
+}
