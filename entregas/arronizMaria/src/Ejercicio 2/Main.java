35c0de03b20ec96b3aa60909a357d5c3832089c7
mariaarroniz <maria.arroniz@alumnos.uneatlantico.es>
2025-02-10 11:41:46 +0100

    Carpetas ok

--- entregas/arronizMaria/src/Ejercicio 2/Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        Frame frame1 = new Frame(4, 4);
-        Frame frame2= new Frame(4, 4);
-        Screen screen = new Screen(frame1,frame2);
-
-        screen.print(); 
-    }
-}
