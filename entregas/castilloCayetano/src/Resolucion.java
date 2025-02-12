465b6dd05559171250b72c01653f48be48cf17a2
MikaOT <castilloruiz321rgmail.com>
2025-02-12 13:20:42 +0100

    Updated Reto-002

---
+++ entregas/castilloCayetano/src/Resolucion.java
@@ -0,0 +1,19 @@
+class Resolucion {
+
+    private int ancho;
+    private int alto;
+
+    public Resolucion(int ancho, int alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    public int obtenerAncho() {
+        return this.ancho;
+    }
+
+    public int obtenerAlto() {
+        return this.alto;
+    }
+
+}
