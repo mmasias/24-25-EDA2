979f1ea936557450a3b4d2a7fdcd88c667ddd166
Oscarpineda3 <pinedaoscar250@gmail.com>
2025-02-12 13:07:48 +0100

    Ejercicio1

---
+++ ejercicios/Ejercicio_1/PantallaEntrelazada.java
@@ -0,0 +1,19 @@
+package ejercicios.Ejercicio_1;
+
+public class PantallaEntrelazada {
+    private Dimension dimension;
+    private ListaCircularFrames listaFrames;
+
+    public PantallaEntrelazada(int ancho, int alto, int tamaño) {
+        this.dimension = new Dimension(ancho, alto);
+        this.listaFrames = new ListaCircularFrames(ancho, alto, tamaño);
+    }
+
+    public void establecerPixel(Coordenada coordenada, int color) {
+        listaFrames.obtenerActual().establecerPixel(coordenada, color);
+    }
+
+    public void renderizar() {
+        listaFrames.obtenerActual().renderizar();
+    }
+}
---
+++ ejercicios/Ejercicio_1/Pixel.java
@@ -0,0 +1,17 @@
+package ejercicios.Ejercicio_1;
+
+public class Pixel {
+    private int color;
+
+    public Pixel(int color) {
+        this.color = color;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}
