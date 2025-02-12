56c219bc4ac90eccc98da2007297f36a7209aa42
Dreyes2907 <dreyb05@gmail.com>
2025-02-12 13:03:46 +0100

    Ejercicio1 hecho

---
+++ ejercicios/Reyes_david/Ejercicio_1/Demo.java
@@ -0,0 +1,13 @@
+package ejercicios.Reyes_david.Ejercicio_1;
+
+public class Demo {
+    public static void main(String[] args) {
+        Dimension dimension = new Dimension(5, 5);
+        GestorPantalla pantalla = new GestorPantalla(dimension);
+
+        pantalla.establecerPixel(new Coordenada(7,2),2);
+        pantalla.establecerPixel(new Coordenada(6,1),2);
+        pantalla.establecerPixel(new Coordenada(5,0),2);
+        pantalla.renderizar();
+    }
+}
