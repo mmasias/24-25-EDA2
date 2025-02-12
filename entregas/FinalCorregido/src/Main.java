6b031af996d6a697bd54c318056ac327e6edb998
maariiooo <mario.viadero@alumnos.uneatlantico.es>
2025-02-12 13:00:01 +0100

    FinalCorregido

--- entregas/FinalCorregido/src/Main.java
+++ entregas/FinalCorregido/src/Main.java
@@ -1,29 +1,79 @@
 public class Main {
     public static void main(String[] args) {
-       /*  Escena escena=new Escena(7, 3, 2);
-         escena.getFrame().pintar();
-        escena.getFrame().establecerPixel(new Coordenada(0, 0), Pixel.MAGENTA);
-        escena.getFrame().establecerPixel(new Coordenada(1, 2), Pixel.BLANCO);
-        escena.getFrame().establecerPixel(new Coordenada(0, 2), Pixel.CYAN);
-        escena.getFrame().pintar();
-        escena.siguiente();
-        escena.getFrame().pintar();
-        escena.siguiente();
-        escena.getFrame().pintar();*/
-        PantallaEntrelazada pantalla= new PantallaEntrelazada(7, 3);
-        pantalla.getEscena().getFrame().establecerPixel(new Coordenada(0, 0), Pixel.CYAN);
-        pantalla.getEscena().siguiente();
-        pantalla.getEscena().getFrame().establecerPixel(new Coordenada(0, 0), Pixel.BLANCO);
-       // pantalla.renderizar();
-
-        PantallaSuperpuesta pantallaSuperpuesta = new PantallaSuperpuesta(7, 3);
-        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(0, 0), Pixel.MAGENTA);
-        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(6, 2), Pixel.BLANCO);
-        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(5, 2), Pixel.MAGENTA);
+        PantallaEntrelazada pantallaEntrelazada = new PantallaEntrelazada(7, 3);
+        
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(0, 0), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(1, 0), Pixel.BLANCO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(2, 0), Pixel.BLANCO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(3, 0), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(4, 0), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(5, 0), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(6, 0), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(0, 1), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(1, 1), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(2, 1), Pixel.CYAN);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(3, 1), Pixel.CYAN);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(4, 1), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(5, 1), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(6, 1), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(0, 2), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(1, 2), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(2, 2), Pixel.BLANCO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(3, 2), Pixel.BLANCO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(4, 2), Pixel.BLANCO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(5, 2), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(6, 2), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().siguiente();
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(0, 0), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(1, 0), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(2, 0), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(3, 0), Pixel.BLANCO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(4, 0), Pixel.BLANCO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(5, 0), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(6, 0), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(0, 1), Pixel.CYAN);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(1, 1), Pixel.CYAN);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(2, 1), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(3, 1), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(4, 1), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(5, 1), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(6, 1), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(0, 2), Pixel.BLANCO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(1, 2), Pixel.BLANCO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(2, 2), Pixel.BLANCO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(3, 2), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(4, 2), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(5, 2), Pixel.NEGRO);
+        pantallaEntrelazada.getEscena().getFrame().establecerPixel(new Coordenada(6, 2), Pixel.NEGRO);
+        pantallaEntrelazada.renderizar();
+        
+        PantallaSuperpuesta pantallaSuperpuesta = new PantallaSuperpuesta(4, 3);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(0, 0), Pixel.NEGRO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(1, 0), Pixel.NEGRO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(2, 0), Pixel.BLANCO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(3, 0), Pixel.NEGRO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(0, 1), Pixel.CYAN);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(1, 1), Pixel.NEGRO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(2, 1), Pixel.NEGRO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(3, 1), Pixel.MAGENTA);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(0, 2), Pixel.BLANCO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(1, 2), Pixel.CYAN);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(2, 2), Pixel.MAGENTA);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(3, 2), Pixel.NEGRO);
         pantallaSuperpuesta.getEscena().siguiente();
+        
         pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(0, 0), Pixel.BLANCO);
-        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(6, 2), Pixel.NEGRO);
-        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(5, 2), Pixel.CYAN);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(1, 0), Pixel.NEGRO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(2, 0), Pixel.NEGRO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(3, 0), Pixel.BLANCO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(0, 1), Pixel.NEGRO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(1, 1), Pixel.MAGENTA);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(2, 1), Pixel.BLANCO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(3, 1), Pixel.NEGRO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(0, 2), Pixel.NEGRO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(1, 2), Pixel.BLANCO);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(2, 2), Pixel.CYAN);
+        pantallaSuperpuesta.getEscena().getFrame().establecerPixel(new Coordenada(3, 2), Pixel.MAGENTA);
         pantallaSuperpuesta.renderizar();
     
     
