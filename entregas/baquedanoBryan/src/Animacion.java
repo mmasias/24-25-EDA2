e8ac1d948364dd579b2383924ae6f34306df5239
enmabry <bryanbaquedano11@gmail.com>
2025-02-16 23:10:24 +0200

    Cambios

--- entregas/baquedanoBryan/src/Animacion.java
+++ entregas/baquedanoBryan/src/Animacion.java
@@ -1,33 +1,36 @@
+import java.util.ArrayList;
+
 public class Animacion {
-    private ListaFrame frames;
+    private ArrayList<Frame> frames;
     private int timer;
+    private int indiceActual;
 
     public Animacion(int timer) {
-        this.frames = new ListaFrame();
+        this.frames = new ArrayList<>();
         this.timer = timer > 0 ? timer : 1000;
+        this.indiceActual = 0;
     }
 
     public void agregarFrame(Frame frame) {
-        frames.agregar(frame);
+        frames.add(frame);
     }
 
     public void reproducir() {
-        if (frames.getTamaño() < 2) {
+        if (frames.size() < 2) {
             System.out.println("Se necesitan al menos dos Frames para animar.");
             return;
         }
-
-        int indice = 0;
 
         while (true) {
             System.out.print("\033[H\033[2J");
             System.out.flush();
 
-            Frame frameIzquierdo = frames.obtener(indice);
-            Frame frameDerecho = frames.obtener((indice + 1) % frames.getTamaño());
+            Frame frameIzquierdo = frames.get(indiceActual);
+            Frame frameDerecho = frames.get((indiceActual + 1) % frames.size());
+
             Pantalla.mostrarFrames(frameIzquierdo, frameDerecho);
 
-            indice = (indice + 1) % frames.getTamaño();
+            indiceActual = (indiceActual + 1) % frames.size(); 
 
             try {
                 Thread.sleep(timer);
