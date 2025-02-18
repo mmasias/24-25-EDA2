b5a37ba7c33926fcf7b43668950ff011186498ea
micastg <micastg@unirioja.es>
2025-02-18 23:46:45 +0100

    02-18 actualizaciob main

--- entregas/castanedaMiguel/02-18/src/BarajaEspanola.java
+++ entregas/castanedaMiguel/02-18/src/BarajaEspanola.java
@@ -1,5 +1,9 @@
 public class BarajaEspanola extends Mazo{
 
+    @Override
+    public void barajar(){
+        System.out.println("barajando baraja española");
+    }
     public void jugarBrisca(){
         System.out.println("jugando a la brisca");
     }
--- entregas/castanedaMiguel/02-18/src/BarajaFrancesa.java
+++ entregas/castanedaMiguel/02-18/src/BarajaFrancesa.java
@@ -1,5 +1,10 @@
 public class BarajaFrancesa extends Mazo{
 
+    @Override
+    public void barajar(){
+        System.out.println("barajando baraja francesa");
+    }
+
     public void jugarPoker(){
         System.out.println("jugando al poker");
     }
