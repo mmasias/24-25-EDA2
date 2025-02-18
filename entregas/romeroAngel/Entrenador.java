f37a6885cc270d1dcfa914686f5c4d9c2adfbaef
Romero <rxmerx504@gmail.com>
2025-02-18 20:22:44 +0100

    Al momento de entrenar un pokemon, cada accion tendra un tiempo aleatorio de espera entre 2-3seg, lo que lo hara un proceso automatico

--- entregas/romeroAngel/Entrenador.java
+++ entregas/romeroAngel/Entrenador.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Entrenador {
@@ -7,9 +8,16 @@ public class Entrenador {
         this.pokemon = pokemon;
     }
 
-    public void entrenar(int sesiones){
+    public void entrenar(int sesiones) throws InterruptedException{
         Scanner scanner = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("¡" + pokemon.getNombre() + " esta listo para entrenar! Presiona Enter para empezar.");
+        scanner.nextLine();
+
+        for(int i = 0;i<sesiones;i++){
+            ordernarAccionAleatoria();
+            
+            Thread.sleep(2000 + new Random().nextInt(1000));
+        }
     }
 
     public void ordernarAccionAleatoria(){
