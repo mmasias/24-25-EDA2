d8e792f3f89972ae5aaba93fd1f9f325da46c740
Romero <rxmerx504@gmail.com>
2025-02-18 20:05:32 +0100

    Se crea la clase Entrenador. Esta se encarga de manejar al pokemon y sus acciones, com el proposito de delegar la responsabilidad a alguien mas y mantener el Main limpio.

---
+++ entregas/romeroAngel/Entrenador.java
@@ -0,0 +1,14 @@
+public class Entrenador {
+    private Pokemon pokemon;
+
+    public Entrenador(Pokemon pokemon){
+        this.pokemon = pokemon;
+    }
+
+    public void entrenar(int sesiones){
+    }
+
+    public void ordernarAccionAleatoria(){
+    }
+
+}
