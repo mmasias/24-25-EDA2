1abfa36337065b97195e124434fe07e4b3f9c49a
MikaOT <castilloruiz321rgmail.com>
2025-02-19 11:20:28 +0100

    Updated Main

--- entregas/castilloCayetano/src/Main.java
+++ entregas/castilloCayetano/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
+        Banco banco = new Banco();
+        banco.realizarOperacion();
+
+        Banco cajero = new Cajero();
+        cajero.realizarOperacion();
+
+        Banco aplicacion = new Aplicacion();
+        aplicacion.realizarOperacion();
+
+
         Interfaz interfaz = new Interfaz();
         interfaz.iniciar();
     }
