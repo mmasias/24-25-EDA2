8e934bf9a90abc50659b97745ab23001ea2863d1
maariiooo <mario.viadero@alumnos.uneatlantico.es>
2025-02-19 13:01:22 +0100

    02/17

--- entregas/viaderoMario/02-17/Main.java
+++ entregas/viaderoMario/02-17/Main.java
@@ -5,10 +5,10 @@ public class Main {
         empleado.iniciarJornada();
 
         if (empleado instanceof Cantante) {
-            ((Cantante) empleado).depurarCodigo();
+            ((Cantante) empleado).escribirCancion();
         }
 
-        Empleado[] empleados = new Cantante()[3];
+        Empleado[] empleados = new Cantante[3];
         empleados[0] = new Cantante();
 
         empleados[1] = new Cantante();
