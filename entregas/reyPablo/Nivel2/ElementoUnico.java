2e4e32c40895253c371253a5124a4efd4eb53fd6
Pablo Rey Ortiz <pablo.reyortiz@unealum.local>
2025-02-26 13:28:10 +0100

    Nivel 2, ElementoUnico

---
+++ entregas/reyPablo/Nivel2/ElementoUnico.java
@@ -0,0 +1,14 @@
+public class ElementoUnico {
+    public static int encontrar(int[] arr) {
+        int unico = 0;
+        for (int num : arr) {
+            unico ^= num;
+        }
+        return unico;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 1, 3, 2};
+        System.out.println(encontrar(arr));
+    }
+}
