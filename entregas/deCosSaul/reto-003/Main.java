36d221b7f7cdb3fe4556c18723994b16af17c606
SdeCos <saul.decossanchez@alumnos.uneatlantico.es>
2025-02-26 13:06:12 +0100

    montana completado

--- entregas/deCosSaul/reto-003/Main.java
+++ entregas/deCosSaul/reto-003/Main.java
@@ -3,13 +3,14 @@ class Main {
 
     numeroFaltante(new int[] { 1, 2, 4, 5, 6 });
 
+    System.out.println(montana(new int[] { 1, 3, 5, 4, 2 }));
   }
 
-  private void sumaObjetivo(int[] numeros, int objetivo) {
+  private static void sumaObjetivo(int[] numeros, int objetivo) {
 
   }
 
-  private void busquedaPalabra(int[] numeros, int objetivo) {
+  private static void busquedaPalabra(int[] numeros, int objetivo) {
 
   }
 
@@ -21,5 +22,35 @@ class Main {
       }
     }
 
+  }
+
+  private static boolean montana(int[] numeros) {
+
+    int indice = 0;
+    boolean creciendo = true;
+    boolean decreciendo = false;
+
+    while (creciendo) {
+      if (indice + 1 == numeros.length) {
+        return true;
+      }
+      if (numeros[indice + 1] > numeros[indice]) {
+      } else {
+        creciendo = false;
+        decreciendo = true;
+      }
+      indice++;
+    }
+
+    while (decreciendo) {
+      if (indice + 1 == numeros.length) {
+        return true;
+      }
+      if (numeros[indice + 1] > numeros[indice]) {
+        return false;
+      }
+      indice++;
+    }
+    return true;
   }
 }
