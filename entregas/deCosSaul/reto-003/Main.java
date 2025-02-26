892aef2bb9e2eb025624c475be203a4602b9ca47
SdeCos <saul.decossanchez@alumnos.uneatlantico.es>
2025-02-26 19:07:49 +0100

    elemento mayoritario

--- entregas/deCosSaul/reto-003/Main.java
+++ entregas/deCosSaul/reto-003/Main.java
@@ -6,6 +6,8 @@ class Main {
     System.out.println(montana(new int[] { 1, 3, 5, 4, 2 }));
 
     System.out.println(puntoDeEquilibrio(new int[] { 1, 2, 3, 4, 6 }));
+
+    System.out.println(elementoMayoritario(new int[] { 3, 2, 2, 2, 4 }));
   }
 
   private static void sumaObjetivo(int[] numeros, int objetivo) {
@@ -56,19 +58,19 @@ class Main {
     return true;
   }
 
-  private static int puntoDeEquilibrio(int[] numero) {
+  private static int puntoDeEquilibrio(int[] numeros) {
     int indice = 1;
     int sumaIzquierda;
     int sumaDerecha;
-    while (indice < numero.length) {
+    while (indice < numeros.length) {
       sumaIzquierda = 0;
       sumaDerecha = 0;
 
       for (int i = 0; i < indice; i++) {
-        sumaIzquierda += numero[i];
+        sumaIzquierda += numeros[i];
       }
-      for (int j = indice + 1; j < numero.length; j++) {
-        sumaDerecha += numero[j];
+      for (int j = indice + 1; j < numeros.length; j++) {
+        sumaDerecha += numeros[j];
       }
       if (sumaIzquierda == sumaDerecha) {
         return indice;
@@ -77,5 +79,34 @@ class Main {
 
     }
     return 0;
+  }
+
+  private static int elementoMayoritario(int[] numeros) {
+
+    int candidato = numeros[0];
+    int contador = 1;
+
+    for (int i = 1; i < numeros.length; i++) {
+      if (contador == 0) {
+        candidato = numeros[i];
+        contador = 1;
+      } else if (candidato == numeros[i]) {
+        contador++;
+      } else {
+        contador--;
+      }
+    }
+
+    contador = 0;
+    for (int num : numeros) {
+      if (num == candidato) {
+        contador++;
+      }
+    }
+
+    if (contador > numeros.length / 2) {
+      return candidato;
+    }
+    return -1;
   }
 }
