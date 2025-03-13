2e4153066e2f9bdd4bd16f4c65f9d33d93c54ada
martin <martin.lopez@alumnos.uneatlantico.es>
2025-03-13 09:08:09 +0100

    trazabilidad implementada

--- entregas/lopezMartin/reto003/ShuntingYard.java
+++ entregas/lopezMartin/reto003/ShuntingYard.java
@@ -10,54 +10,60 @@ public class ShuntingYard {
         System.out.println("Ingrese una expresión matemática en notación infija:");
         String expresionInfija = sc.nextLine();
         
-        System.out.println(shuntingYard(expresionInfija));
+        System.out.println(shuntingYard(expresionInfija, sc));
     }
     
-    public static String shuntingYard(String expresionInfija) {
+    public static String shuntingYard(String expresionInfija, Scanner sc) {
         Stack <Character> pila = new Stack<>();
         Stack <Character> salida = new Stack<>();
         char[] tokens = tokenizar(expresionInfija);
 
-        for(int i= 0; i<tokens.length; i++){
-            if (esNumero(tokens[i])){
+        for(int i = 0; i < tokens.length; i++) {
+            System.out.println("Procesando token: " + tokens[i]);
+            
+            if (esNumero(tokens[i])) {
                 salida.push(tokens[i]);
-            }else{
+            } else {
                 if (esOperador(tokens[i])) {
-                    while(!pila.isEmpty() && esOperador(pila.peek()) && precedencia(pila.peek()) >= precedencia(tokens[i]) ){
+                    while(!pila.isEmpty() && esOperador(pila.peek()) && precedencia(pila.peek()) >= precedencia(tokens[i])) {
                         salida.push(pila.pop());
                     }
                     pila.push(tokens[i]);
-                }else if(tokens[i]=='('){
+                } else if (tokens[i] == '(') {
                     pila.push(tokens[i]);
-                }else{
-                    while(pila.peek()!='('){
+                } else {
+                    while (pila.peek() != '(') {
                         salida.push(pila.pop());
                     }
                     pila.pop();
                 }
             }
+            
+            imprimirEstadoPilas(pila, salida);
+            System.out.println("Presiona ENTER para continuar...");
+            sc.nextLine();
         }
 
-        while(!pila.isEmpty()){
+        while (!pila.isEmpty()) {
             salida.push(pila.pop());
+            imprimirEstadoPilas(pila, salida);
+            System.out.println("Presiona ENTER para continuar...");
+            sc.nextLine();
         }
 
         String resultado = "";
         while (!salida.isEmpty()) {
-        resultado = salida.pop() + resultado;
+            resultado = salida.pop() + resultado;
         }
 
         return resultado;
-         
     }
     
     private static char[] tokenizar(String expresionInfija) {
         char[] caracteres = new char[expresionInfija.length()];
-        
         for (int i = 0; i < expresionInfija.length(); i++) {
             caracteres[i] = expresionInfija.charAt(i);
         }
-        
         return caracteres;
     }
 
@@ -83,5 +89,10 @@ public class ShuntingYard {
                 return -1;
         }
     }
-
+    
+    private static void imprimirEstadoPilas(Stack<Character> pila, Stack<Character> salida) {
+        System.out.println("Pila de operadores: " + pila);
+        System.out.println("Pila de salida: " + salida);
+        System.out.println("----------------------");
+    }
 }
