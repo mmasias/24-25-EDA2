6409591f867fcc9a8771d05b693c17b2271b705d
mariaarroniz <maria.arroniz@alumnos.uneatlantico.es>
2025-03-16 13:27:24 +0100

    Fixed ()

--- entregas/arronizMaria/shuttingYard/ShutingYard.java
+++ entregas/arronizMaria/shuttingYard/ShutingYard.java
@@ -4,16 +4,13 @@ import java.util.Stack;
 class ShutingYard {
     public static void main(String[] args) {
         String input = intro();
+        System.out.println("Operación en infijo: " + input);
 
         ShutingYard example1 = new ShutingYard();
-        Stack<String> rpn = example1.InfijoAPostfijo(input);
-
-        System.out.println(input);
-        System.out.println(rpn);
-
+        example1.InfijoAPostfijo(input);
     }
 
-    Stack<String> InfijoAPostfijo(String input) {
+    void InfijoAPostfijo(String input) {
         Stack<String> operadores = new Stack<>();
         Stack<String> salida = new Stack<>();
 
@@ -22,55 +19,69 @@ class ShutingYard {
         for (int index = 0; index < tokens.length; index++) {
             String token = tokens[index];
 
-            if (token.isNumber(token)) {
+            if (isNumber(token)) {
                 salida.push(token);
             }
 
-            if (token.isOp(token)) {
-                do {
+            if (isOp(token)) {
+                while (!operadores.isEmpty() && isOp(operadores.peek()) && priority(operadores.peek()) >= priority(token)) {
                     salida.push(operadores.peek());
                     operadores.pop();
-
-                } while (!operadores.isEmpty() && isOp(operadores.peek()) && operadores.peek().priority()>=token.priority());
-                
+                }
                 operadores.push(token);
             }
 
             if (token.equals("(")) {
-                salida.push(token);
+                operadores.push(token);
             }
 
             if (token.equals(")")) {
-                do {
+                if (!operadores.isEmpty()) {
                     salida.push(operadores.peek());
                     operadores.pop();
-
-                } while (operadores.peek().equals("("));
-                
-                operadores.pop();
+                }
+                while (!operadores.isEmpty() && operadores.peek().equals("(")) {
+                    operadores.pop();
+                }
             }
-
-            do {
-                salida.push(operadores.peek());
-                operadores.pop();
-            } while (!operadores.isEmpty());
         }
-        return salida;
+
+        while (!operadores.isEmpty()) {
+            salida.push(operadores.peek());
+            operadores.pop();
+        }
+
+        System.out.println("Operación en postfijo: " + salida);
     }
 
     static boolean isNumber(String token) {
+        return token.equals("0") || token.equals("1") || token.equals("2") || token.equals("3") || token.equals("4")
+                || token.equals("5") || token.equals("6") || token.equals("7") || token.equals("8")
+                || token.equals("9");
 
     }
 
     static boolean isOp(String token) {
-        
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    static int priority(String token) {
+        switch (token) {
+            case "*":
+                return 3;
+            case "/":
+                return 2;
+            case "+":
+                return 1;
+            case "-":
+                return 1;
+        }
+
+        return 0;
 
     }
 
-    static int priority(){
-
-    }
-
+    @SuppressWarnings("resource")
     static String intro() {
         System.out.println("Escribe la operacion a transformar: ");
         return new Scanner(System.in).nextLine();
