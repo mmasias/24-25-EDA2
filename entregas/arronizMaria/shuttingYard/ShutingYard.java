8442e05954c8529521aebf8c763daaffca46ee83
mariaarroniz <maria.arroniz@alumnos.uneatlantico.es>
2025-03-16 12:25:39 +0100

    Almost

--- entregas/arronizMaria/shuttingYard/ShutingYard.java
+++ entregas/arronizMaria/shuttingYard/ShutingYard.java
@@ -6,48 +6,68 @@ class ShutingYard {
         String input = intro();
 
         ShutingYard example1 = new ShutingYard();
-        example1.InfijoAPostfijo(input);
+        Stack<String> rpn = example1.InfijoAPostfijo(input);
+
+        System.out.println(input);
+        System.out.println(rpn);
+
     }
 
-    void InfijoAPostfijo(String input) {
+    Stack<String> InfijoAPostfijo(String input) {
         Stack<String> operadores = new Stack<>();
         Stack<String> salida = new Stack<>();
 
         String[] tokens = input.split("");
 
         for (int index = 0; index < tokens.length; index++) {
-
             String token = tokens[index];
 
             if (token.isNumber(token)) {
                 salida.push(token);
-            } else {
-                while(!operadores.isEmpty() && operadores.peek().isOp() && ){
+            }
+
+            if (token.isOp(token)) {
+                do {
                     salida.push(operadores.peek());
-                    operadores.pop(operadores.peek());
-                }
+                    operadores.pop();
+
+                } while (!operadores.isEmpty() && isOp(operadores.peek()) && operadores.peek().priority()>=token.priority());
+                
                 operadores.push(token);
             }
 
-            if (token == "(") {
+            if (token.equals("(")) {
                 salida.push(token);
             }
 
-            if (token == ")") {
-                while (salida.peek() != "(") {
+            if (token.equals(")")) {
+                do {
+                    salida.push(operadores.peek());
+                    operadores.pop();
 
-                }
-
+                } while (operadores.peek().equals("("));
+                
+                operadores.pop();
             }
 
+            do {
+                salida.push(operadores.peek());
+                operadores.pop();
+            } while (!operadores.isEmpty());
         }
+        return salida;
     }
 
     static boolean isNumber(String token) {
 
     }
 
-    static boolean isOp() {
+    static boolean isOp(String token) {
+        
+
+    }
+
+    static int priority(){
 
     }
 
