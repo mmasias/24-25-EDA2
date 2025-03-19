941c9d11349fe82881365d5f3824dbeac12c1fb1
enmabry <bryanbaquedano11@gmail.com>
2025-03-19 13:38:50 +0100

    Se ha creado el metodo detenerse

--- entregas/baquedanoBryan/Reto1903/AscensorRecursivo.java
+++ entregas/baquedanoBryan/Reto1903/AscensorRecursivo.java
@@ -4,52 +4,49 @@ public class AscensorRecursivo {
     
     private int plantaActual;
     private int plantaDestino;
-    private int dirección;
     private boolean estado;
 
-
-    public AscensorRecursivo(int plantaActual, int plantaDestino) {
+    // Constructor solo con planta actual
+    public AscensorRecursivo(int plantaActual) {
         this.plantaActual = plantaActual;
-        this.plantaDestino = plantaDestino;  
-        this.dirección = calculoDirección();  
         this.estado = true;   
     }
 
-    
-    public void moverse() {
-        if (plantaActual == plantaDestino) {
-            detenerse(); 
-            return;
-        }
+    // Método que mueve el ascensor hasta la planta destino
+    public void moverse(int plantaDestino) {
+        this.plantaDestino = plantaDestino;  // Ahora la planta destino se asigna aquí
 
-        System.out.println("Estoy en el piso" + plantaActual);
+       
+
+        System.out.println("Estoy en el piso " + plantaActual);
     
-        if (calculoDirección()>0) {
-            plantaActual++; 
+        if (calculoDirección() > 0) {
+            plantaActual++;
             System.out.println("Subiendo al piso " + plantaActual);
         } else {
-            plantaActual--; 
+            plantaActual--;
             System.out.println("Bajando al piso " + plantaActual);
         }
-    
-        moverse();
+
+        moverse(plantaDestino);  // Llamada recursiva con el destino
     }
 
-
     public void detenerse() {
-        estado = false;
-        System.out.println("Ascensor detenido en el piso " + plantaActual);
+
+        if(plantaActual == plantaDestino){
+            estado = false;
+            System.out.println("Ascensor detenido en el piso " + plantaActual);
+        }
+       
     }
 
     public int calculoDirección() {
         return Integer.compare(plantaDestino, plantaActual);
     }
 
-    
 
-    
     public static void main(String[] args) {
-        AscensorRecursivo ascensor = new AscensorRecursivo(0, 5);
-        ascensor.moverse();
+        AscensorRecursivo ascensor = new AscensorRecursivo(1);  
+        ascensor.moverse(5); 
     }
 }
