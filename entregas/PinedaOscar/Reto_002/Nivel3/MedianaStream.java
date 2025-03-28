f930b4b73f9f1590023d8c1a557ba8808147b844
Oscarpineda3 <pinedaoscar250@gmail.com>
2025-03-28 13:15:36 +0100

    Implementacion Cada Nivel

---
+++ entregas/PinedaOscar/Reto_002/Nivel3/MedianaStream.java
@@ -0,0 +1,35 @@
+package entregas.PinedaOscar.Reto_002.Nivel3;
+
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+public class MedianaStream {
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+    public void agregarNumero(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double encontrarMediana() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        } else {
+            return maxHeap.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        MedianaStream stream = new MedianaStream();
+        int[] numeros = {1, 3, 2, 4, 5};
+        for (int num : numeros) {
+            stream.agregarNumero(num);
+            System.out.println("Mediana actual: " + stream.encontrarMediana());
+        }
+    }
+}
